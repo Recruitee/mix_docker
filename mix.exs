d03442242c84c684f152b750fde34ec9a5c23342
Tymon Tobolski <i@teamon.eu>
2017-04-04 20:46:33 +0200

    v0.4.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MixDocker.Mixfile do
   def project do
     [
       app: :mix_docker,
-      version: "0.3.2",
+      version: "0.4.0",
       elixir: "~> 1.3",
       build_embedded: Mix.env == :prod,
       start_permanent: Mix.env == :prod,
